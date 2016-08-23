ff0a1a8252e813d36dd26c69976b2064e9b14107
Jared Palmer <jaredpalmer>
2016-08-23 12:27:03 -0400

    tenant middleware

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(express.static('public'))
+app.use((req, res, next) => {
+  req.set('tenant') = req.headers.host.domain.split['.'][0]
+  next()
+})
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 app.get('/api/me', (req, res) => {
-  const tenant = req.get('X-My-Custom-Param-1') || 'no tenant'
+  const tenant = req.get('tenant') || 'no tenant'
   console.log(tenant)
   res.send(tenant)
 })
