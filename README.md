6623c7d47717e364b502a1327834986602f24d40
Jared Palmer <jaredpalmer>
2016-08-22 20:28:36 -0400

    reset

--- README.md
+++
@@ -1,2 +0,0 @@
-# twork-api
-wip
--- db/index.js
+++
@@ -1,7 +0,0 @@
-import neo4j from 'neo4j'
-import Bluebird from 'bluebird'
-const db = new neo4j.GraphDatabase(process.env.GRAPHENEDB_URL)
-
-db.cypher = Bluebird.promisify(db.cypher)
-
-export default db
