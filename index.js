39d9cfa2fcf554e004429418c348e9d2bbdb3104
Jared Palmer <jaredpalmer>
2016-08-23 12:28:46 -0400

    fix req.set

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.static('public'))
 app.use((req, res, next) => {
-  req.set('tenant') = req.headers.host.domain.split['.'][0]
+  req.tenant = req.headers.host.domain.split['.'][0]
   next()
 })
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 app.get('/api/me', (req, res) => {
-  const tenant = req.get('tenant') || 'no tenant'
-  console.log(tenant)
-  res.send(tenant)
+  res.send(req.tenant || 'no tenant found')
 })
 
 const server = http.createServer(app)
