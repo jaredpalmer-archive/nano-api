a49bfa6e20b3440c71308deabe4dba007ba6755b
Jared Palmer <jaredpalmer>
2016-08-23 12:09:56 -0400

    heroku wildcard

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const http = require('http')
 const morgan = require('morgan')
-const proxy = require('http-proxy')
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.static('public'))
-
 app.get('/api', (req, res) => {
-  console.log(req.get('X-My-Custom-Param-1'));
   res.send('Hello world!')
 })
 app.get('/api/me', (req, res) => {
   const tenant = req.get('X-My-Custom-Param-1') || 'no tenant'
+  console.log(tenant)
   res.send(tenant)
 })
 
 const server = http.createServer(app)
 
-server.listen(process.env.PORT || 3000, (err) => {
+server.listen(process.env.PORT || 5000, (err) => {
   if (err) console.log(err)
-  console.log("server listening")
+  console.log("server listening on port 5000")
 })
